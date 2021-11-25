cc0ac015cfa9ab149b1b1997102fceb855336865
Michael Kitas <mixaliskitas@gmail.com>
2021-11-25 22:01:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpass'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: "toemail@gmail.com",
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
